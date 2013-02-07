5feb72d08486723cf93290a14b0c9d1b32abec08
Jeremy McAnally <jmcanally@github.com>
2013-02-07 14:41:28 -0500

    Remove commented out code and fixed TODO

--- lib/mathemagical.rb
+++ lib/mathemagical.rb
@@ -7,28 +7,13 @@ require "strscan"
 require "htmlentities"
 
 module Mathemagical
-  # TODO FIX THIS WARNING
   VERSION = '0.0.1' unless defined?(:VERSION)
 
 	class Error < StandardError; end
 
-	# Borrowed from eim_xml.rb
 	def self.encode(s)
-  	# s.to_s.gsub(/[&\"\'<>]/) do |m|
-   #  	case m
-   #  	when "&"
-   #  		"&amp;"
-   #  	when '"'
-   #  		"&quot;"
-   #  	when "'"
-   #  		"&apos;"
-   #  	when "<"
-   #  		"&lt;"
-   #  	when ">"
-   #  		"&gt;"
-   #  	end
-   #  end
     @entities ||= HTMLEntities.new
+
     # TODO: HTMLEntities Y U DOUBLE ENCODE??
     @entities.encode(s).gsub(/&amp;([#a-zA-Z0-9]{2,24});/, '&\1;')
   end
